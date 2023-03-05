d23ac558c079cc978cd314eb058155b8ed0e44ba
Alice Uu <alice@owl.ooo>
2023-03-05 13:54:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function drawAll()
     ctx.fillStyle = "rgb(0, 0, 0)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.strokeStyle = "rgb(255, 255, 255)";
+    ctx.fillStyle = "rgb(255, 255, 255)";
     ctx.font = "20px sans-serif";
-    ctx.strokeText("Hello world", 0, 0);
+    ctx.fillText("Hello world", 0, 100);
 }
 
 setInterval(drawAll, 16.7);
