196d9d48fd8207426846b9e60c271c8fb37a3a62
Alice Uu <alice@owl.ooo>
2023-03-05 14:49:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var canvas       = document.getElementById("Canvas");
-var ctx          = canvas.getContext("2d");
+var canvas         = document.getElementById("Canvas");
+var ctx            = canvas.getContext("2d");
+var touchesStarted = 0;
+var touchesEnded   = 0;
 
 function drawAll()
 {
@@ -8,7 +10,15 @@ function drawAll()
 
     ctx.fillStyle = "rgb(255, 255, 255)";
     ctx.font = "20px sans-serif";
-    ctx.fillText("Hello world", 0, 100);
+    ctx.fillText("Touches Started: " + touchesStarted, 0, 30);
+    ctx.fillText("Touches Ended: " + touchesEnded, 0, 60);
 }
 
+window.addEventListener("touchmove", (evt) => {});
+window.addEventListener("touchstart", (evt) => {
+    touchesStarted++;
+});
+window.addEventListener("touchend", (evt) => {
+    touchesEnded++;
+});
 setInterval(drawAll, 16.7);
