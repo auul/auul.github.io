ab5a81035fb11f284d3dbbddc031787e7eb02add
Alice Uu <alice@owl.ooo>
2023-03-05 16:08:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 var canvas  = document.getElementById("Canvas");
 var ctx     = canvas.getContext("2d");
-var touches = null;
+
+const firstTouch = [0, 0];
+const lastTouch  = [0, 0];
+const touches    = [null, null];
 
 function getTouches(evt)
 {
+    switch (evt.targetTouches.length) {
+    case 0:
+        touches[0] = null;
+        touches[1] = null;
+        break;
+    case 1:
+        touches[0] = firstTouch;
+        touches[1] = null;
+        break;
+    default:
+        touches[0] = firstTouch;
+        touches[1] = lastTouch;
+        break;
+    }
+
+    /*
     touches = new Array(evt.targetTouches.length);
     for (let i = 0; i < evt.targetTouches.length; i++) {
         touches[i] = [evt.targetTouches[i].pageX - canvas.offsetLeft,
                       evt.targetTouches[i].pageY - canvas.offsetTop];
     }
+    */
 }
 
 function drawAll()
@@ -18,10 +38,8 @@ function drawAll()
 
     ctx.fillStyle = "rgb(255, 255, 255)";
     ctx.font = "20px sans-serif";
-    if (touches) {
-        for (let i = 0; i < touches.length; i++) {
-            ctx.fillText("Touch " + i + ": " + touches[i][0] + ", " + touches[i][1], 0, i * 30 + 30);
-        }
+    for (let i = 0; touches[i]; i++) {
+        ctx.fillText("Touch " + i + ": " + touches[i][0] + ", " + touches[i][1], 0, i * 30 + 30);
     }
 }
 
